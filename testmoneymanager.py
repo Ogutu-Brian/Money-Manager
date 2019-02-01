365c31c52952b20643ed3a9c8c59b0226a956df7
Ogutu Brian <codingbrian58@gmail.com>
2019-02-01 12:35:11 +0300

    Add requirements.txt

--- testmoneymanager.py
+++ testmoneymanager.py
@@ -2,41 +2,36 @@ import unittest
 
 from moneymanager import MoneyManager
 
+
 class TestMoneyManager(unittest.TestCase):
 
     def setUp(self):
         # Create a test BankAccount object
         self.user = MoneyManager()
 
-        # Provide it with some initial balance values        
+        # Provide it with some initial balance values
         self.user.balance = 1000.0
 
     def test_legal_deposit_works(self):
         # Your code here to test that depsositing money using the account's
         # 'deposit_funds' function adds the amount to the balance.
-        
 
     def test_illegal_deposit_raises_exception(self):
         # Your code here to test that depositing an illegal value (like 'bananas'
         # or such - something which is NOT a float) results in an exception being
         # raised.
-        
 
     def test_legal_entry(self):
         # Your code here to test that adding a new entry with a a legal amount subtracts the
         # funds from the balance.
-        
 
     def test_illegal_entry_amount(self):
         # Your code here to test that withdrawing an illegal amount (like 'bananas'
         # or such - something which is NOT a float) raises a suitable exception.
 
-
-        
     def test_illegal_entry_type(self):
         # Your code here to test that adding an illegal entry type (like 'bananas'
         # or such - something which is NOT a float) raises a suitable exception.
-        
 
     def test_insufficient_funds_entry(self):
         # Your code here to test that you can only spend funds which are available.
@@ -45,6 +40,5 @@ class TestMoneyManager(unittest.TestCase):
         # should be raised and the withdrawal should NOT be applied to the user balance
         # or the user's transaction list.
 
-
-# Run the unit tests in the above test case
-unittest.main()       
+        # Run the unit tests in the above test case
+unittest.main()

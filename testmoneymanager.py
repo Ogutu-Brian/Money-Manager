cf7efbaeb16024d322599761c33558232ed5f0b5
Ogutu Brian <codingbrian58@gmail.com>
2019-02-02 19:18:09 +0300

    Add test for legal entry and update documentation string

--- testmoneymanager.py
+++ testmoneymanager.py
@@ -10,16 +10,21 @@ class TestMoneyManager(unittest.TestCase):
         self.user.balance = 1000.0
 
     def test_legal_deposit_works(self):
-        '''Tests if correct procedure for depositing funds works'''
+        '''Tests that depositing money using the account's deposit funds function
+        adds the amount to the balance'''
         self.user.deposit_funds(1000)
         self.assertEqual(2000, self.user.balance)
 
     def test_illegal_deposit_raises_exception(self):
-        """Tests for deposit that is not float"""
+        '''Tests that that depositing a value that is not a float results into 
+        an exception being raised'''
         self.assertEqual(self.user.deposit_funds("4dssjd"), False)
 
     def test_legal_entry(self):
-        pass
+        '''Tests that adding a new entry with a legal amount subtracts the funds from 
+        the balance'''
+        self.user.add_entry(999, item_types[0])
+        self.assertEqual(self.user.balance, 1)
 
     def test_illegal_entry_amount(self):
         pass
